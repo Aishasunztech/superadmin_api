4ab26329f6a4efc9edd86bd21438bc56f32821d7
abaidmalik243 <abaidmalik243@gmail.com>
2019-06-21 13:30:19 +0500

    modify Device status api

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1497,6 +1497,7 @@ exports.checkComponent = async function (req, res) {
 }
 
 exports.offlineDevices = async function (req, res) {
+    console.log('offline devices get: ');
     let devicesQ = `SELECT devices.*, white_labels.name as whitelabel FROM devices LEFT JOIN white_labels ON (devices.whitelabel_id = white_labels.id)`;
     let devices = await sql.query(devicesQ);
     devices.forEach((device) => {
@@ -1518,6 +1519,7 @@ exports.offlineDevices = async function (req, res) {
         res.send({
             status: false,
             msg: "No data found",
+            devices: []
         })
     }
 
@@ -1558,17 +1560,20 @@ exports.deviceStatus = async function (req, res) {
     try {
         let updateQ = '';
         if (start_date && expiry_date && id && requiredStatus == Constants.DEVICE_EXTEND) {
-            updateQ = `UPDATE devices SET start_date= '${start_date}', expiry_date = '${expiry_date}', remaining_days = '2' WHERE id = ${id}`;
-            console.log('update query is: ', updateQ);
+            let status = 'expired';
+            var varDate = new Date(expiry_date);
+            var today = new Date();
 
+            if (varDate > today) {
+                status = 'active';
+            }
+            // console.log('status is: ', status);
+
+            updateQ = `UPDATE devices SET start_date= '${start_date}', status = '${status}', expiry_date = '${expiry_date}', remaining_days = '2' WHERE id = ${id}`;
         } else if (id && requiredStatus == Constants.DEVICE_ACTIVATED) {
             updateQ = `UPDATE devices SET account_status= '', status='active' WHERE id = ${id}`;
-            console.log('deviceStatus update query is: ', updateQ);
-
         } else if (id && requiredStatus == Constants.DEVICE_SUSPENDED) {
             updateQ = `UPDATE devices SET account_status= 'suspended' WHERE id = ${id}`;
-            console.log('deviceStatus update query is: ', updateQ);
-
         } else {
             res.send({
                 status: false,
@@ -1576,7 +1581,8 @@ exports.deviceStatus = async function (req, res) {
             })
         }
         if (updateQ != '') {
-            sql.query(updateQ, async function (err, rslts) {
+            console.log('deviceStatus update query is: ', updateQ);
+            await sql.query(updateQ, async function (err, rslts) {
                 if (err) {
                     console.log(err);
                     res.send({
@@ -1584,10 +1590,41 @@ exports.deviceStatus = async function (req, res) {
                         msg: "Error occur"
                     });
                 } else {
-                    res.send({
-                        status: true,
-                        msg: "update account_status successfully"
+                    let selectQuery = `SELECT devices.*, white_labels.name as whitelabel FROM devices LEFT JOIN white_labels ON (devices.whitelabel_id = white_labels.id) WHERE devices.id = ${id}`;
+                    console.log('select query: ', selectQuery);
+                    await sql.query(selectQuery, async function (err, devices) {
+                        console.log('selectQuery result is: ', devices);
+                        if (err) {
+                            console.log(err);
+                            res.send({
+                                status: false,
+                                msg: "Error occur"
+                            });
+                        } else if (devices.length) {
+                            devices.forEach((device) => {
+                                device.finalStatus = device_helpers.checkStatus(device)
+
+                                device.whitelabel = general_helpers.checkValue(device.whitelabel);
+                                device.fl_dvc_id = general_helpers.checkValue(device.fl_dvc_id)
+                                device.wl_dvc_id = general_helpers.checkValue(device.wl_dvc_id)
+                                device.status = general_helpers.checkValue(device.status)
+                                device.mac_address = general_helpers.checkValue(device.mac_address)
+                                device.serial_number = general_helpers.checkValue(device.serial_number);
+                            })
+                            res.send({
+                                status: true,
+                                devices: devices,
+                                msg: "Offline Device Status Successfully Updated!"
+                            })
+                        } else {
+                            res.send({
+                                status: false,
+                                devices: [],
+                                msg: "Failed to update Offline Device Status!",
+                            })
+                        }
                     });
+
                 }
 
             });
@@ -1599,11 +1636,10 @@ exports.deviceStatus = async function (req, res) {
         }
     } catch (error) {
         console.log(error);
-        data = {
+        res.send({
             status: false,
             msg: "exception for deviceStatus",
-        };
-        res.send(data);
+        });
         return;
     }
 
@@ -1633,7 +1669,7 @@ exports.updateDeviceStatus = async function (req, res) {
 
 
 
-    
+
 
 }
 
@@ -1801,46 +1837,46 @@ exports.savePackage = async function (req, res) {
 
 
 
-exports.getPrices = async function(req, res) {
+exports.getPrices = async function (req, res) {
     let whitelebel_id = req.params.whitelabel_id;
     let sim_id = {};
     let chat_id = {};
     let pgp_email = {};
     let vpn = {};
-    if(whitelebel_id){
-        let selectQuery = "SELECT * FROM prices WHERE whitelabel_id='"+whitelebel_id+"'";
+    if (whitelebel_id) {
+        let selectQuery = "SELECT * FROM prices WHERE whitelabel_id='" + whitelebel_id + "'";
         sql.query(selectQuery, async (err, reslt) => {
-             if(err) throw err;
-             if(reslt){
+            if (err) throw err;
+            if (reslt) {
                 //  console.log('result for get prices are is ', reslt);
-               
-                 if(reslt.length){
-                     for(let item of reslt){
-                         if(item.price_for == 'sim_id'){
+
+                if (reslt.length) {
+                    for (let item of reslt) {
+                        if (item.price_for == 'sim_id') {
                             sim_id[item.price_term] = item.unit_price
-                         }else if(item.price_for == 'chat_id'){
+                        } else if (item.price_for == 'chat_id') {
                             chat_id[item.price_term] = item.unit_price
-                        }else if(item.price_for == 'pgp_email'){
+                        } else if (item.price_for == 'pgp_email') {
                             pgp_email[item.price_term] = item.unit_price
-                        }else if(item.price_for == 'vpn'){
+                        } else if (item.price_for == 'vpn') {
                             vpn[item.price_term] = item.unit_price
                         }
-                     }
-                     let data = {
-                         sim_id: sim_id ? sim_id : {},
-                         chat_id: chat_id ? chat_id : {},
-                         pgp_email: pgp_email ? pgp_email : {},
-                         vpn: vpn ? vpn : {}
-                     }
-                     res.send({
+                    }
+                    let data = {
+                        sim_id: sim_id ? sim_id : {},
+                        chat_id: chat_id ? chat_id : {},
+                        pgp_email: pgp_email ? pgp_email : {},
+                        vpn: vpn ? vpn : {}
+                    }
+                    res.send({
                         status: true,
                         msg: "Data found",
                         data: data
-                        
+
                     })
-                 }
-                
-             }else{
+                }
+
+            } else {
                 let data = {
                     sim_id: sim_id ? sim_id : {},
                     chat_id: chat_id ? chat_id : {},
@@ -1848,14 +1884,14 @@ exports.getPrices = async function(req, res) {
                     vpn: vpn ? vpn : {}
                 }
 
-                 res.send({
-                     status: true,
-                     msg: "Data found",
-                     data: data
-                 })
-             }
+                res.send({
+                    status: true,
+                    msg: "Data found",
+                    data: data
+                })
+            }
         })
-    }else{
+    } else {
 
         let data = {
             sim_id: sim_id ? sim_id : {},
@@ -1877,9 +1913,9 @@ exports.checkPackageName = async function (req, res) {
 
     try {
         let name = req.body.name !== undefined ? req.body.name : null;
-      
+
         let checkExistingQ = "SELECT pkg_name FROM packages WHERE pkg_name='" + name + "'";
-      
+
         let checkExisting = await sql.query(checkExistingQ);
         console.log(checkExistingQ, 'query is')
         if (checkExisting.length) {
